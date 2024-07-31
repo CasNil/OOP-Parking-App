176850224f68effde63503b5eaa7b19f9f3d9bbc
Casper Nilsson <caspernilsson13@gmail.com>
2024-07-31 18:04:45 +0200

    ReservationDaoImpl and ParkingSpotDaoImpl implemented

--- src/main/java/se/lexicon/dao/ParkingSpotDaoImpl.java
+++ src/main/java/se/lexicon/dao/ParkingSpotDaoImpl.java
@@ -1,5 +1,67 @@
 package se.lexicon.dao;
 
-public class ParkingSpotDaoImpl {
+import se.lexicon.model.ParkingSpot;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class ParkingSpotDaoImpl implements ParkingSpotDao {
+
+    private List<ParkingSpot> parkingSpots = new ArrayList<>();
+
+    @Override
+    public ParkingSpot create(ParkingSpot parkingSpot) {
+        for (ParkingSpot parkingSpotElement : parkingSpots) {
+            if (parkingSpotElement.getSpotNumber() == parkingSpot.getSpotNumber() && parkingSpotElement.getAreaCode() == parkingSpot.getAreaCode()) {
+                throw new IllegalArgumentException("Parking spot already exists");
+            }
+        }
+        parkingSpots.add(parkingSpot);
+        return parkingSpot;
+    }
+
+    @Override
+    public Optional<ParkingSpot> find(int spotNumber, int areaCode) {
+        for (ParkingSpot parkingSpot : parkingSpots) {
+            if (parkingSpot.getSpotNumber() == spotNumber && parkingSpot.getAreaCode() == areaCode) {
+                return Optional.of(parkingSpot);
+            }
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public List<ParkingSpot> findByAreaCode(int areaCode) {
+        List<ParkingSpot> foundSpots = new ArrayList<>();
+        for (ParkingSpot parkingSpot : parkingSpots) {
+            if (parkingSpot.getAreaCode() == areaCode) {
+                foundSpots.add(parkingSpot);
+            }
+        }
+        return foundSpots;
+    }
+
+    @Override
+    public void occupyParkingSpot(int spotNumber, int areaCode) {
+        for (ParkingSpot parkingSpotElement : parkingSpots) {
+            if (parkingSpotElement.getSpotNumber() == spotNumber && (parkingSpotElement.getAreaCode() == areaCode)) {
+                parkingSpotElement.setSpotNumber(spotNumber);
+                parkingSpotElement.setAreaCode(areaCode);
+                parkingSpotElement.occupy();
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void vacateParkingSpot(int spotNumber, int areaCode) {
+        for (ParkingSpot parkingSpotElement : parkingSpots) {
+            if (parkingSpotElement.getSpotNumber() == spotNumber && (parkingSpotElement.getAreaCode() == areaCode)) {
+                parkingSpotElement.vacate();
+                break;
+            }
+        }
+    }
     // todo: implement ParkingSpotDao
 }
--- src/main/java/se/lexicon/dao/ReservationDaoImpl.java
+++ src/main/java/se/lexicon/dao/ReservationDaoImpl.java
@@ -1,5 +1,34 @@
 package se.lexicon.dao;
 
-public class ReservationDaoImpl {
+import se.lexicon.model.Reservation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class ReservationDaoImpl implements ReservationDao {
+
+    private List<Reservation> reservationList = new ArrayList<>();
+
+    @Override
+    public Reservation create(Reservation reservation) {
+        for (Reservation reservationElement : reservationList) {
+            if (reservationElement.getId().equalsIgnoreCase(reservation.getId()) && reservationElement.getAssociatedVehicle().equals(reservation.getAssociatedVehicle())) {
+                throw new IllegalArgumentException("You have already made a reservation");
+            }
+        }
+        reservationList.add(reservation);
+        return reservation;
+    }
+
+    @Override
+    public Optional<Reservation> find(String id) {
+        for (Reservation reservation : reservationList) {
+            if (reservation.getId().equalsIgnoreCase(id)) {
+                return Optional.of(reservation);
+            }
+        }
+        return Optional.empty();
+    }
     // todo: implement ReservationDao
 }
